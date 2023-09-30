37c35b323996a95866b65116b6a57bd8c132d6ab
qiangdadi <hxq205732204@163.com>
2023-09-30 14:01:22 +0800

    create the Tree class and Node struct

---
+++ Tree/src/test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "../include/Tree.h"
+
+void Test01(){
+    struct Node<int>* root = new Node<int>();
+    root->val = 5;
+    root->parent = nullptr;
+    root->left = nullptr;
+    root->right = nullptr;
+
+    Tree<int> t(root);
+    if(t.getRoot()->val == 5){
+        std::cout << "Test succeed!\n";
+    }else{
+        std::cout << "Test filed!\n";
+    }
+}
+
+int main(){
+    Test01();
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,1 @@
+# This is a repository about data structure and basic algorithm
